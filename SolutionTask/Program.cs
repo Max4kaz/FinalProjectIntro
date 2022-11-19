a21391f60bc63b7fe4ebaf6dab8c6d04288c594b
Maxim <max4kaz@gmail.com>
2022-11-19 15:56:28 +0700

    Add solution of the task

---
+++ SolutionTask/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+string[] initialArray = { "hello", "2", "world", ":-)" };   // { "hello", "2", "world", ":-)" } ||  { "1234", "1567", "-2", "computer science" } ||  { "Russia", "Denmark", "Kazan" }
+int amountSymbols = 3;
+int countString = CountStringLessAmountSymbols(initialArray, amountSymbols);
+string[] formedArray = FormStringArray(initialArray, countString, amountSymbols);
+Console.WriteLine("Исходный массив: ");
+PrintArray(initialArray);
+Console.WriteLine($"Сформированный массив, из строк длина которых меньше или равна {amountSymbols} символа: ");
+PrintArray(formedArray);
+
+int CountStringLessAmountSymbols(string[] array, int number)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= number) count++;
+    }
+    return count;
+}
+
+string[] FormStringArray(string[] array, int count, int number)
+{
+    string[] result = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= number)
+        {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}. ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
